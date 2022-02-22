95b6fae0f7a6a9bb1c831411ca21d0d295aef2d0
Victoria Bermudez <vmbermudez044@gmail.com>
2022-02-22 08:26:10 -0500

    Initial Commit

---
+++ ScopeIt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScopeIt
+//
+//  Created by Victoria Bermudez (student LM) on 2/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScopeIt/ScopeItApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScopeItApp.swift
+//  ScopeIt
+//
+//  Created by Victoria Bermudez (student LM) on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ScopeItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
