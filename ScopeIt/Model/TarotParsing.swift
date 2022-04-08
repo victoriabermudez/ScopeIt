e822da5504e20b8a4ee9fc5d846991ae2ce8e6e6
Michelle Kelly <s015580@students.lmsd.org>
2022-04-08 08:17:18 -0400

    update tarot

--- ScopeIt/Model/TarotParsing.swift
+++ ScopeIt/Model/TarotParsing.swift
@@ -1,50 +1,112 @@
-////
-////  TarotParsing.swift
-////  ScopeIt
-////
-////  Created by Michelle Kelly (student LM) on 4/4/22.
-////
 //
-//import Foundation
+
+//  TarotParsing.swift
+
+//  ScopeIt
+
 //
+
+//  Created by Michelle Kelly (student LM) on 4/4/22.
+
 //
-//class FetchDataTarot : ObservableObject{
+
+
+
+import Foundation
+
+
+
+class FetchDataTarot : ObservableObject{
+
+
+
+    @Published var responsesTarot : ResponseTarot = ResponseTarot()
+
+    
+        init(){
+
+            print(String(self.responsesTarot.cards.count) ?? "akjfns")
+
+            guard let url = URL(string: "https://rws-cards-api.herokuapp.com/api/v1/cards/random?n=10") else {
+
+                print("no data")
+
+                return}
+
+
+
+            URLSession.shared.dataTask(with: url) { (data, responseTarot, errors) in
+
+                guard let data = data else {
+
+                   print("urlsessionshared")
+
+                    return
+
+                }
+
+
+
+
+
+                let decoder = JSONDecoder()
+
+                if let responseTarot = try? decoder.decode(ResponseTarot.self, from: data) {
+
+                    DispatchQueue.main.async {
+
+                        self.responsesTarot = responseTarot
+                        //self.responsesTarot = responseTarot
+                        print(self.responsesTarot.cards[0].name ?? "non")
+                        //Name = self.responsesTarot.cards[0].name
+                    }
+
+                }
+
+                else{
+
+                    print("Can't decode tarot JSON")
+
+                }
+
+                print(String(self.responsesTarot.cards.count+2) ?? "akjfns")
+
+            }.resume() //if you don't have this it'll just schedule it it won't actually execute
+            print(String(self.responsesTarot.cards.count + 1) ?? "akjfns")
+    }
+
+}
+
+
+
+
+
+struct ResponseTarot: Codable{
+
+    var cards : [Card] = [Card]()
+
+}
+
+
+
+//struct Cards: Codable{
 //
-//    @Published var responsesTaor : ResponseTarot = ResponseTarot()
-//        init(){
-//            guard let url = URL(string: "https://www.affirmations.dev") else {
-//                print("no data")
-//                return}
+//    var card: Card = Card()
 //
-//            URLSession.shared.dataTask(with: url) { (data, responseAffirmation, errors) in
-//                guard let data = data else {
-//                   print("urlsessionshared")
-//                    return
-//                }
-//
-//
-//                let decoder = JSONDecoder()
-//                if let responseAffirmation = try? decoder.decode(ResponseAffirmation.self, from: data) {
-//                    DispatchQueue.main.async {
-//                        self.responsesAffirmation = responseAffirmation
-//                    }
-//                }
-//                else{
-//                    print("Can't decode JSON")
-//                }
-//
-//            }.resume() //if you don't have this it'll just schedule it it won't actually execute
-//    }
 //}
-//
-//
-//struct ResponseAffirmation: Codable{
-//    var affirmation : String?
-//}
-//
-//
-//extension ResponseAffirmation : Identifiable{
-//    var id: String {return affirmation!}
-//}
-//
-//
+
+
+
+struct Card: Codable{
+
+    var name: String?
+
+}
+
+
+
+extension Card : Identifiable{
+
+    var id: String {return name!}
+
+}
--- ScopeIt/Views/DailyView.swift
+++ ScopeIt/Views/DailyView.swift
@@ -21,35 +21,40 @@ import SwiftUI
 
 
 struct DailyView: View {
-
-    @StateObject var fetchDataAffirmation = FetchDataAffirmation()
-
-    @StateObject var fetchDataAdvice = FetchDataAdvice()
-
     
-
+    @StateObject var fetchDataAffirmation = FetchDataAffirmation()
+    
+    @StateObject var fetchDataAdvice = FetchDataAdvice()
+    
+    @StateObject var fetchDataTarot = FetchDataTarot()
+    
+    
     var body: some View {
-
+        
         VStack{
-
-        Text(fetchDataAffirmation.responsesAffirmation.affirmation ?? "no affirmation")
-
-        Text(fetchDataAdvice.responsesAdvice.result.slip.advice ?? "no advice")
-
+            
+            Text(fetchDataAffirmation.responsesAffirmation.affirmation ?? "no affirmation")
+            
+            //Text(fetchDataAdvice.responsesAdvice.result.slip.advice ?? "no advice")
+            
+            //Text(Name ?? "non")
+           //Text(fetchDataTarot.responsesTarot.cards[0].name ?? "non")
+            ///?? "no tarot")
+            
         }
-
+        
     }
-
+    
 }
 
 
 
 struct DailyView_Previews: PreviewProvider {
-
+    
     static var previews: some View {
-
+        
         DailyView()
-
+        
     }
-
+    
 }
